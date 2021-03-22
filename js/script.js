b140bf8480ad108acbfab969deb0482d14a8d82b
nobuhiro-0714 <79903720+nobuhiro-0714@users.noreply.github.com>
2021-03-23 00:26:03 +0900

    commit 4

--- js/script.js
+++ js/script.js
@@ -4,15 +4,12 @@ initPhotoSwipeFromDOM(".js-my-gallery");
 $(function () {
 
   //iOS対策
-  //iOSでは疑似要素を含むaタグのリンクは２回タップしないと遷移とページ内スクロールをしないため、
-  //ユーザーエージェント判定でiOSの場合はbodyタグにiosを付与し、対象の疑似要素をdisplay: noneする
   var ua = navigator.userAgent;
   if (/iPhone|iPad|iPod/.test(ua)) {
     $("body").addClass("ios");
   }
 
   //Worksのリンクを有効化
-  //スライド（Swiper）内に記載のリンクを有効にするため下記の記述が必要 (;´･ω･)ｳｰﾝ･･･
   $(".works-url").on("click", "a", function (e) {
     e.stopPropagation();
   });
